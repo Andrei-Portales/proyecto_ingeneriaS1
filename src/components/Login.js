781bedeb72f9fa317e2198087f74d6da4f21865a
edman <edman@LAPTOP-IUQHGQ9D>
2021-04-20 13:33:26 -0600

    Login Component

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/css/login.css';
+
+function Login() {
+    return (
+        <div className="login">
+            <p>Login</p>
+        </div>
+    );
+}
+
+export default Login;
