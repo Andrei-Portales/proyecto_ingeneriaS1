169bea525eb7807d9c37bf170d5fd88be54392fd
edman <edman@LAPTOP-IUQHGQ9D>
2021-04-20 14:15:41 -0600

    Cambios

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,11 +2,18 @@ import React from 'react';
 import '../assets/css/login.css';
 
 function Login() {
-    return (
-        <div className="login">
-            <p>Login</p>
-        </div>
-    );
+  return (
+    <div className="login">
+      <p>Login</p>
+      <form>
+        <input type="text" placeholder="Usuario" />
+        <br/>
+        <br/>
+        <input type="password" placeholder="Contraseña"/><br/><br/>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 export default Login;
