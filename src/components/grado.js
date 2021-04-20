82ff70c6ded305f70aab500d6eceaeda96898fce
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2021-04-20 12:26:28 -0600

    Components

---
+++ src/components/grado.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './grado.css';
+
+function grado() {
+    return (
+        <div className="gradoc">
+            <p> Esto es un grado</p>
+        </div>
+    );
+}
+
+export default grado;
--- src/containers/app.js
+++ src/containers/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Grado from '../components/grado'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>ReactApp</h1>
+        <Grado />
       </div>
     );
   }
