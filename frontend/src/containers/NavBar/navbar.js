808f097cb89c8c6c8ed90a66585f7a6fa9e477d0
edman <edman@LAPTOP-IUQHGQ9D>
2021-04-27 17:49:22 -0600

    Arreglar  dropdown de botón materias

--- frontend/src/containers/NavBar/navbar.js
+++ frontend/src/containers/NavBar/navbar.js
@@ -1,51 +1,46 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import './navbar.css';
 import { Link } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import Logo from '../images/logoCompleto.png';
 
 class navbar extends React.Component {
-    render() {
-        return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">              
-                <img class="navbar-brand" src={Logo} />
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                    <Link to="/grados">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">GRADOS <span class="sr-only"></span></a>
-                        </li>
-                    </Link>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                        MATERIAS
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <Link to="/materia">
-                                <a class="dropdown-item" href="#">CUARTO</a>
-                            </Link>
-                            <Link to="/materia">
-                                <a class="dropdown-item" href="#">QUINTO</a>
-                            </Link>
-                            <Link to="/materia">
-                                <a class="dropdown-item" href="#">SEXTO</a>
-                            </Link>
-                        </div>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">EJERCICIOS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">OFFLINE</a>
-                    </li>
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">              
+        <img className="navbar-brand" src={Logo} />
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <Link to="/grados">
+              <a className="nav-link" href="#">GRADOS <span class="sr-only"></span></a>
+            </Link>
+            <DropdownButton variant="link" id="dropdown-basic-button" title="MATERIAS">
+              <Link to="/materia">
+                <Dropdown.Item href="#/action-1">CUARTO</Dropdown.Item>
+              </Link>
+              <Link to="/materia">
+                <Dropdown.Item href="#/action-1">QUINTO</Dropdown.Item>
+              </Link>
+              <Link to="/materia">
+                <Dropdown.Item href="#/action-1">SEXTO</Dropdown.Item>
+              </Link>
+            </DropdownButton>
+            <li className="nav-item">
+              <a className="nav-link" href="#">EJERCICIOS</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">OFFLINE</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
   
 export default navbar;
--- frontend/src/containers/app.js
+++ frontend/src/containers/app.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import Grado from '../components/grado';
 import Login from './Login/Login';
 import Register from './Register/Register';
 import Materia from './Materias/Materias';
