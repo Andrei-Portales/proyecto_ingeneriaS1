863cef1de29268ff8019ff559a78d882b77e0136
Andrei Portales <andreifan123@gmail.com>
2021-05-04 17:36:32 -0600

    proyecto en backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 // import rutas
 const {
@@ -27,6 +28,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 // Login requests
 app.post('/login', (req, res, next) => login(req, res, next, pool)); // email, password
 app.post('/register', (req, res, next) => register(req, res, next, pool)); //  email, name, password
@@ -38,9 +43,14 @@ app.post('/confirm_recovery_code', (req, res, next) => confirmRecoveryCode(req, 
 
 app.post('/insert-tema', (req, res, next)=>insertTema(req, res, next, pool)); // id_grado, title, body, video_url
 app.post('/get-temas', (req, res, next)=> getTemas(req, res, next, pool)); // id_materia
-app.get('/get-grados', (req, res, next)=> getGrados(req, res, next, pool));  
+app.post('/get-grados', (req, res, next)=> getGrados(req, res, next, pool));  
 app.post('/materias-grado', (req, res, next)=> materiasGrado(req, res, next, pool)); // grado
 app.post('/get-tema', (req, res, next)=> getTema(req, res, next, pool)); // id
+
+
+app.get('*', (req, res, next)=>{
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
 
