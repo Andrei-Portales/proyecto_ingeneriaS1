8384e2a89922da0e5412db22f4612e6f31ef28cc
Estuardo <xavihernand7@hotmail.com>
2021-08-04 22:51:58 -0600

    Update Contacto Page

--- frontend-2/src/pages/Contacto/Contacto.js
+++ frontend-2/src/pages/Contacto/Contacto.js
@@ -1,9 +1,79 @@
 import styles from './Contacto.module.scss';
+import { useSelector } from "react-redux";
 
 const Contacto = () => {
+  const isLightTheme = useSelector((state) => state.theme.theme) === "LIGHT";
+
+  const title = `${styles.title} ${
+    !isLightTheme && styles["title-dark"]
+  }`;
+
+  const prf = `${styles.prf} ${
+    !isLightTheme && styles["prf-dark"]
+  }`;
+
     return (
         <div className={styles.contactoPage}>
-          <h1 className={styles.title}>Contacto</h1>
+
+          <h1 className={title}>Contacto</h1>
+          <br></br><br></br>
+
+          <h7 className={prf}>Haz tus consultas a través de nuestros correos.</h7>
+          <br></br><br></br>
+
+          <div className={styles.wrap}>
+            <div className={styles.contactoForm}>
+              <h2 className={styles.subtitle}>
+                Nombre
+              </h2>
+              <h2 className={styles.subtitle}>
+                Correo
+              </h2>
+              
+              <h5 className={styles.content}>
+                Alfredo Quezada
+              </h5>
+              <h5 className={styles.content}>
+                que191002@uvg.edu.gt
+              </h5>
+
+              <h5 className={styles.content}>
+                Andrei Portales
+              </h5>
+              <h5 className={styles.content}>
+                por19825@uvg.edu.gt
+              </h5>
+
+              <h5 className={styles.content}>
+                Javier Hernández
+              </h5>
+              <h5 className={styles.content}>
+                her19202@uvg.edu.gt
+              </h5>
+
+              <h5 className={styles.content}>
+                Marco Ramírez
+              </h5>
+              <h5 className={styles.content}>
+                ram19588@uvg.edu.gt
+              </h5>
+
+              <h5 className={styles.content}>
+                Mirka Monzón
+              </h5>
+              <h5 className={styles.content}>
+                mon18139@uvg.edu.gt
+              </h5>
+
+              <h5 className={styles.content}>
+                Oscar de León
+              </h5>
+              <h5 className={styles.content}>
+                del19298@uvg.edu.gt
+              </h5>
+            </div>
+          </div>
+
         </div>
     );
 };
