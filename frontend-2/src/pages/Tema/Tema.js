644d5cc6c7cc2df565f2ea6cdd608ee7a2cde0cc
Oscar <owdelc@gmail.com>
2021-09-26 20:20:17 -0600

    Ticket: Eliminar o Utilizar variables sin uso en Componente Tema.js

--- frontend-2/src/pages/Tema/Tema.js
+++ frontend-2/src/pages/Tema/Tema.js
@@ -1,40 +1,50 @@
-import { useState, useRef, useEffect, useCallback } from 'react';
-import { useParams } from 'react-router-dom';
-import { useHistory } from 'react-router';
-import { useSelector } from 'react-redux';
-// import TemaItemSuggested from '../../components/TemaItem/TemaItemSuggested';
-import styles from './Tema.module.scss';
-import { tema as linkTema, downloadTema } from '../../util/links';
+import { useState, useRef, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
+import { useHistory } from "react-router";
+import { useSelector } from "react-redux";
 
-import { UilImport } from '@iconscout/react-unicons';
-import { UilClipboard } from '@iconscout/react-unicons';
-import { PDFExport, savePDF } from '@progress/kendo-react-pdf';
+// import TemaItemSuggested from '../../components/TemaItem/TemaItemSuggested'; ha sido por no ser utilizao en el codigo actual
+
+import styles from "./Tema.module.scss";
+import { tema as linkTema } from "../../util/links";
+
+import { UilImport } from "@iconscout/react-unicons";
+import { UilClipboard } from "@iconscout/react-unicons";
+
+// import savePDF from "@progress/kendo-react-pdf" ha sido eliminado por no ser utilizado en el codigo actual
+import { PDFExport } from "@progress/kendo-react-pdf";
 
 const Tema = () => {
   const params = useParams();
   const history = useHistory();
-  const isLightTheme = useSelector((state) => state.theme.theme) === 'LIGHT';
+  const isLightTheme = useSelector((state) => state.theme.theme) === "LIGHT";
+
+  /* setShowContent no es utilizado pero es esencial para el 
+  funcionamiento del estado, el cual si es utilizado en el codigo*/
   const [showContent, setShowContent] = useState(true);
+
   const [showDescription, setShowDescription] = useState(false);
   const pdfExportComponent = useRef(null);
   const [tema, setTema] = useState(null);
 
   const mainClases = `${
     showContent ? styles.videoSection : styles.fullScreenMain
-  } ${!isLightTheme ? styles['main-dark'] : ''}`;
+  } ${!isLightTheme ? styles["main-dark"] : ""}`;
 
   const materiaClasses = `${styles.materia} ${
-    !isLightTheme && styles['materia-dark']
+    !isLightTheme && styles["materia-dark"]
   }`;
 
   const tituloTema = `${styles.tituloTema} ${
-    !isLightTheme && styles['titulo-tema-dark']
+    !isLightTheme && styles["titulo-tema-dark"]
   }`;
 
   const suggestions = `${styles.suggestionSection} ${
-    !isLightTheme ? styles['more-dark'] : ''
+    !isLightTheme ? styles["more-dark"] : ""
   }`;
 
+
+  // onClickTemaHandler ha sido eliminado por falta de uso dentro del codigo
   // const onClickTemaHandler = (id) => {
   //   history.push(`/grados/${params.grado}/${params.materia}/${id}`);
   // };
@@ -42,9 +52,9 @@ const Tema = () => {
   const fetchTemaData = useCallback(async () => {
     try {
       const response = await fetch(linkTema, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ id: params.id }),
       });
@@ -113,34 +123,31 @@ const Tema = () => {
               <div>
                 <div
                   className={styles.description}
-                  style={{ height: showDescription ? 'fit-content' : '200px' }}
+                  style={{ height: showDescription ? "fit-content" : "200px" }}
                   dangerouslySetInnerHTML={{ __html: tema.body }}
                 ></div>
                 <button
                   onClick={toggleShowDescription}
                   className={styles.showMoreDescription}
                 >
-                  {showDescription ? 'Mostrar menos' : 'Mostrar mas'}
+                  {showDescription ? "Mostrar menos" : "Mostrar mas"}
                 </button>
               </div>
             </PDFExport>
           </div>
 
           <div className={styles.opcionesContenedor}>
-            <a
+            <button
               className={styles.botonDescargar}
-              href={`${downloadTema}/${params.id}`}
-              target="_blank"
-              rel="noreferrer"
-              download
+              onClick={exportPDFWithComponent}
             >
-              Descargar PDF <UilImport size="16"></UilImport>{' '}
-            </a>
+              Descargar PDF <UilImport size="16"></UilImport>{" "}
+            </button>
             <button
               className={styles.botonEjercicios}
               onClick={() => onClickExercice(params.id)}
             >
-              Ejercicios <UilClipboard size="16"></UilClipboard>{' '}
+              Ejercicios <UilClipboard size="16"></UilClipboard>{" "}
             </button>
           </div>
 
