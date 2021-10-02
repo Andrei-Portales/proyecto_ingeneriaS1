0ae46b5cd9f45b6ca0e2e128fb3358d6b1c0c061
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2021-10-02 00:54:18 -0600

    Pestaña de inicio suspendida

--- frontend-2/src/components/Navigation/Navigation.js
+++ frontend-2/src/components/Navigation/Navigation.js
@@ -14,102 +14,102 @@ import logo from "../../assets/logoCompleto.png";
 import { Fragment } from "react";
 
 const Navigation = () => {
-  const history = useHistory();
-  const location = useLocation();
-  const dispatch = useDispatch();
-  const [showDrawer, setShowDrawer] = useState(false);
+    const history = useHistory();
+    const location = useLocation();
+    const dispatch = useDispatch();
+    const [showDrawer, setShowDrawer] = useState(false);
 
-  const themeIsLight = useSelector((state) => state.theme.theme) === "LIGHT";
-  const isAuth = useSelector((state) => state.auth.isLoggedIn);
-  const isAdmin = useSelector((state) => state.auth.isAdmin);
+    const themeIsLight = useSelector((state) => state.theme.theme) === "LIGHT";
+    const isAuth = useSelector((state) => state.auth.isLoggedIn);
+    const isAdmin = useSelector((state) => state.auth.isAdmin);
 
-  const toggleThemeHandler = () => dispatch(themeActions.toggleTheme());
-  const logoutHandler = () => dispatch(authActions.logout());
-  const addHandler = () => history.push("/add");
+    const toggleThemeHandler = () => dispatch(themeActions.toggleTheme());
+    const logoutHandler = () => dispatch(authActions.logout());
+    const addHandler = () => history.push("/add");
 
-  const toggleshowDrawer = () => setShowDrawer((prevState) => !prevState);
+    const toggleshowDrawer = () => setShowDrawer((prevState) => !prevState);
 
-  // Definicion de clases para tema
+    // Definicion de clases para tema
 
-  const navClases = `${styles.nav} ${!themeIsLight ? styles["nav-dark"] : ""}`;
+    const navClases = `${styles.nav} ${!themeIsLight ? styles["nav-dark"] : ""}`;
 
-  return (
-    <Fragment>
-      <div className={navClases}>
-        <div className={styles["nav-left"]}>
-          <img alt="logo" src={logo} className={styles.logo} />
+    return (
+        <Fragment>
+            <div className={navClases}>
+                <div className={styles["nav-left"]}>
+                    <img alt="logo" src={logo} className={styles.logo} />
 
-          {isAuth && (
-            <nav className={styles.actions}>
-              <ul>
-                <NavLink
-                  to="/"
-                  exact
-                  activeClassName={styles["action-selected"]}
-                  className={styles.action}
-                >
-                  Inicio
-                </NavLink>
-                <NavLink
-                  to="/grados"
-                  activeClassName={styles["action-selected"]}
-                  className={styles.action}
-                >
-                  Grados
-                </NavLink>
-                <NavLink
-                  to="/contacto"
-                  activeClassName={styles["action-selected"]}
-                  className={styles.action}
-                >
-                  Contacto
-                </NavLink>
-                <NavLink
-                  to="/preguntas-frecuentes"
-                  activeClassName={styles["action-selected"]}
-                  className={styles.action}
-                >
-                  Preguntas frecuentes
-                </NavLink>
-                <NavLink
-                  to="/about-us"
-                  activeClassName={styles["action-selected"]}
-                  className={styles.action}
-                >
-                  Acerca de Nosotros
-                </NavLink>
-                <div className={styles.action} onClick={toggleThemeHandler}>
-                  {themeIsLight ? <UilMoon size="22" /> : <UilSun size="22" />}
+                    {isAuth && (
+                        <nav className={styles.actions}>
+                            <ul>
+                                {/* <NavLink
+                                    to="/"
+                                    exact
+                                    activeClassName={styles["action-selected"]}
+                                    className={styles.action}
+                                >
+                                    Inicio
+                                </NavLink> */}
+                                <NavLink
+                                    to="/grados"
+                                    activeClassName={styles["action-selected"]}
+                                    className={styles.action}
+                                >
+                                    Grados
+                                </NavLink>
+                                <NavLink
+                                    to="/contacto"
+                                    activeClassName={styles["action-selected"]}
+                                    className={styles.action}
+                                >
+                                    Contacto
+                                </NavLink>
+                                <NavLink
+                                    to="/preguntas-frecuentes"
+                                    activeClassName={styles["action-selected"]}
+                                    className={styles.action}
+                                >
+                                    Preguntas frecuentes
+                                </NavLink>
+                                <NavLink
+                                    to="/about-us"
+                                    activeClassName={styles["action-selected"]}
+                                    className={styles.action}
+                                >
+                                    Acerca de Nosotros
+                                </NavLink>
+                                <div className={styles.action} onClick={toggleThemeHandler}>
+                                    {themeIsLight ? <UilMoon size="22" /> : <UilSun size="22" />}
+                                </div>
+                            </ul>
+                        </nav>
+                    )}
                 </div>
-              </ul>
-            </nav>
-          )}
-        </div>
 
-        {isAuth && (
-          <div className={styles["nav-right"]}>
-            {isAdmin && location.pathname !== "/add" && (
-              <button className={styles.add} onClick={addHandler}>
-                Agregar tema
-              </button>
-            )}
-            <button className={styles.logout} onClick={logoutHandler}>
-              Cerrar sesion
-            </button>
-          </div>
-        )}
+                {isAuth && (
+                    <div className={styles["nav-right"]}>
+                        {isAdmin && location.pathname !== "/add" && (
+                            <button className={styles.add} onClick={addHandler}>
+                                Agregar tema
+                            </button>
+                        )}
+                        <button className={styles.logout} onClick={logoutHandler}>
+                            Cerrar sesion
+                        </button>
+                    </div>
+                )}
 
-        {isAuth && (
-          <div className={styles.drawerIcon} onClick={toggleshowDrawer}>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        )}
-      </div>
-      {showDrawer && isAuth && <Drawer toggleDrawer={toggleshowDrawer} />}
-    </Fragment>
-  );
+                {isAuth && (
+                    <div className={styles.drawerIcon} onClick={toggleshowDrawer}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                )}
+            </div>
+            {showDrawer && isAuth && <Drawer toggleDrawer={toggleshowDrawer} />}
+        </Fragment>
+    );
 };
 
 export default Navigation;
--- frontend-2/src/index.js
+++ frontend-2/src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import Context from "./store/context";
 import useExerciseGlobal from "./store/useExerciseGlobal";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { Grados } from "./pages/Grados/Grados.js";
 
 const Index = () => {
   const stor = useExerciseGlobal();
