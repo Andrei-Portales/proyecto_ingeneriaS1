0a441159dae5c571c659f749e66f72846e392268
Mirka <mirkanicolle@gmail.com>
2021-10-14 12:01:11 -0600

    Register form

--- frontend-2/src/components/RegisterForm/RegisterForm.js
+++ frontend-2/src/components/RegisterForm/RegisterForm.js
@@ -1,132 +1,102 @@
-import { useReducer, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import LoadingSpinnerSized from '../LoadingSpinnerSized/LoadingSpinnerSized';
-import styles from './RegisterForm.module.scss';
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import LoadingSpinnerSized from "../LoadingSpinnerSized/LoadingSpinnerSized";
+import styles from "./RegisterForm.module.scss";
+import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  auth,
+  registerWithEmailAndPassword,
+  signInWithGoogle,
+} from "../../firebase";
 
-
-const initialState = {
-  email: '',
-  name: '',
-  password: '',
-  cPassword: '',
-};
-
-const reducer = (state, action) => {
-  const type = action.type;
-
-  if (type === 'EMAIL') {
-    return { ...state, email: action.value };
-  } else if (type === 'NAME') {
-    return { ...state, name: action.value };
-  } else if (type === 'PASSWORD') {
-    return { ...state, password: action.value };
-  } else if (type === 'CPASSWORD') {
-    return { ...state, cPassword: action.value };
-  }
-
-  return initialState;
-};
-
-const RegisterForm = (props) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+function RegisterForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCpassword] = useState("");
+  const [name, setName] = useState("");
+  const [user, loading] = useAuthState(auth);
   const history = useHistory();
   const [isLoading, setIsLoading] = useState(false);
 
-  const submitHandler = async (event) => {
-    event.preventDefault();
-    
-    try{
-      setIsLoading(true);
-      // const response = await fetch('url', {
-      //   method: 'POST',
-      //   body: JSON.stringify({ email, password }),
-      // });
-  
-      // response.json();
-      await new Promise(resolve => setTimeout(resolve, 2000));
-
-      setIsLoading(false);
-      history.replace('/login');
-    }catch(e){
-      setIsLoading(false);
-    }
+  const register = () => {
+    if (!name) alert("Please enter name");
+    registerWithEmailAndPassword(name, email, password);
   };
 
-  const hasAccountHandler = () => history.replace('/login');
+  const hasAccountHandler = () => history.replace("/login");
 
-  const emailChangeHandler = (event) => {
-    dispatch({
-      type: 'EMAIL',
-      value: event.target.value,
-    });
-  };
-
-  const nameChangeHandler = (event) => {
-    dispatch({
-      type: 'NAME',
-      value: event.target.value,
-    });
-  };
-
-  const passwordChangeHandler = (event) => {
-    dispatch({
-      type: 'PASSWORD',
-      value: event.target.value,
-    });
-  };
-
-  const cPasswordChangeHandler = (event) => {
-    dispatch({
-      type: 'CPASSWORD',
-      value: event.target.value,
-    });
-  };
+  useEffect(() => {
+    if (loading) return;
+    if (user) history.replace("/");
+  }, [user, loading, history]);
 
   return (
-    <form onSubmit={submitHandler} className={styles.form}>
-      <h2>Registrarse</h2>
-      <input
-        className={styles.input}
-        placeholder="Correo electronico"
-        type="email"
-        name="email"
-        value={state.email}
-        onChange={emailChangeHandler}
-      />
-      <input
-        className={styles.input}
-        placeholder="Nombre"
-        name="name"
-        value={state.ename}
-        onChange={nameChangeHandler}
-      />
-      <input
-        className={styles.input}
-        placeholder="Contraseña"
-        type="password"
-        value={state.password}
-        onChange={passwordChangeHandler}
-      />
-      <input
-        className={styles.input}
-        placeholder="Confirmar Contraseña"
-        type="password"
-        value={state.cPassword}
-        onChange={cPasswordChangeHandler}
-      />
+    <div>
+      <div className={styles.divForm}>
+        <ToastContainer />
+        <form className={styles.form}>
+          {/* <h2>Registrarse</h2> */}
+          <input
+            className={styles.input}
+            placeholder="Correo electronico"
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className={styles.input}
+            placeholder="Nombre"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            className={styles.input}
+            placeholder="Contraseña"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            className={styles.input}
+            placeholder="Confirmar Contraseña"
+            type="password"
+            value={cpassword}
+            onChange={(e) => setCpassword(e.target.value)}
+          />
+        </form>
 
-     {!isLoading && <button className={styles.submit}>Registrarse</button>}
-     {isLoading && <LoadingSpinnerSized height='40px' width='40px' border='3px'/>}
-
-     {!isLoading && <div>
-        <span>¿Ya tienes una cuenta?</span>
-        <span className={styles.register} onClick={hasAccountHandler}>
-          {' '}
-          Iniciar sesion
-        </span>
-      </div>}
-    </form>
+        {!isLoading && (
+          <button onClick={register} className={styles.submit}>
+            Registrarse
+          </button>
+        )}
+        {isLoading && (
+          <LoadingSpinnerSized height="40px" width="40px" border="3px" />
+        )}
+      </div>
+      <div className={styles.socialRegister}>
+        <div className={styles.socialLoginTitle}>Registrarse con</div>
+        <ul>
+          <li>
+            <button onClick={signInWithGoogle}>Google</button>
+          </li>
+        </ul>
+      </div>
+      {!isLoading && (
+        <div className="w-100 text-center mt-2">
+          <span>¿Ya tienes una cuenta?</span>
+          <span className={styles.register} onClick={hasAccountHandler}>
+            {" "}
+            Iniciar sesion
+          </span>
+        </div>
+      )}
+    </div>
   );
-};
+}
 
 export default RegisterForm;
